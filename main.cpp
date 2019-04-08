31af65519a7a0a9f9e90d618c7f2dc94b634ad7e
Tony Fiallos <afiallosh@gmail.com>
2019-04-08 23:17:10 +0200

    Added glfw lib

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <GLFW/glfw3.h>
+
+static void GlfwErrorCallback(int error, const char* description)
+{
+	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
 
 int main(int argc, char** argv)
 {
+	glfwSetErrorCallback(GlfwErrorCallback);
 	printf("Hello world\n");
 	return 1;
 }
