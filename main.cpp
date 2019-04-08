6234af5c4d4901d8540ebca5ab3dac4d6767a3ff
Tony Fiallos <afiallosh@gmail.com>
2019-04-08 23:32:13 +0200

    Empty window with glfw

--- main.cpp
+++ main.cpp
@@ -9,6 +9,24 @@ static void GlfwErrorCallback(int error, const char* description)
 int main(int argc, char** argv)
 {
 	glfwSetErrorCallback(GlfwErrorCallback);
-	printf("Hello world\n");
-	return 1;
+	if (!glfwInit()) return 1;
+	GLFWwindow* window = glfwCreateWindow(900, 900, "hilbertgl", NULL, NULL);
+	if (window == NULL) return 1;
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1); // Enable vsync
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		glClearColor(0.45f, 0.55f, 1.00f, 1.00f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
 }
